b59bc279fe4c81928e4e1dc7ac54ddb35d3bb26d
Julian De Tomasi <juliandetomasi@gmail.com>
2016-02-28 13:18:35 -0300

    avances intro y mejora de notas

--- slides/compile.sh
+++ slides/compile.sh
@@ -5,6 +5,7 @@ mkdir $TMP_DIR
 cp slides.tex $TMP_DIR/notes.tex
 sed -i "s/documentclass.*/\documentclass[notes=only]{beamer}/g" $TMP_DIR/notes.tex
 cd $TMP_DIR
+cp ../czt.sty .
 pdflatex notes.tex
 cp notes.pdf ../
 cd ..
