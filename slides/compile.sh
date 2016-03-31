6d1b00422825b193adb4450dd0c3094ad63614ad
Julian De Tomasi <juliandetomasi@gmail.com>
2016-03-30 21:48:34 -0300

    Actualizo slides

--- slides/compile.sh
+++ slides/compile.sh
@@ -3,6 +3,7 @@ TMP_DIR=temp_notes_123f
 echo "Compiling Notes..."
 mkdir $TMP_DIR
 cp slides.tex $TMP_DIR/notes.tex
+cp -r img $TMP_DIR/img
 sed -i "s/documentclass.*/\documentclass[notes=only]{beamer}/g" $TMP_DIR/notes.tex
 cd $TMP_DIR
 cp ../czt.sty .
